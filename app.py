2916d3787bbf7e1e99265defc15bebbe3bdc5e75
Kiran Songire <122895248+KiranSongire@users.noreply.github.com>
2026-05-07 22:18:59 +0000

    new file

--- app.py
+++ app.py
@@ -1,3 +1,11 @@
+import asyncio
+
+try:
+    asyncio.get_running_loop()
+except RuntimeError:
+    loop = asyncio.new_event_loop()
+    asyncio.set_event_loop(loop)
+
 import streamlit as st
 from dotenv import load_dotenv
 import os
